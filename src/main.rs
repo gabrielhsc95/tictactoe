7fd9e313a1534df410c1ee076f0b92a1ceb3c78c
Gabriel Cardoso <gabrielhsc95@gmail.com>
2024-10-03 21:20:54 -0500

    not close immediately

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod coordinates;
 mod player;
 mod terminal;
 mod tictactoe;
+use std::io;
 use terminal::Terminal;
 use tictactoe::TicTacToe;
 
@@ -10,4 +11,9 @@ fn main() {
     let terminal = Terminal::new();
     let mut tic_tac_toe = TicTacToe::new(terminal);
     tic_tac_toe.play();
+    println!("Press Enter to exit...");
+    match io::stdin().read_line(&mut String::new()) {
+        Ok(_) => {}
+        Err(error) => println!("Error reading input: {}", error),
+    }
 }
